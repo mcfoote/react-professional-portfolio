88284e4689e50a3c341b8994dc6e2dcdd55c57d1
Mitchell Foote <mcfoote321@gmail.com>
2023-01-30 15:31:11 -0500

    work on data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,46 @@
 
 export const projects = [
-    
+    {
+        title: '2d Game Engine',
+        subtitle: 'C++, SDL2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-1.png',
+        link: 'https://github.com/mcfoote/2d-game-engine',
+    },
+    {
+        title: 'DOOM WASM clone',
+        subtitle: 'C, WASM',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-2.png',
+        link: 'https://github.com/mcfoote/selfdrivingcarNeuralNetwork',
+    },
+    {
+        title: 'Self Driving Car Simulator',
+        subtitle: 'JS/HTML/CSS, Canvas',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-3.png',
+        link: 'https://github.com/mcfoote/selfdrivingcarNeuralNetwork',
+    },
+    {
+        title: 'ChatterBox',
+        subtitle: 'MERN stack',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-4.png',
+        link: 'https://github.com/mcfoote/Chatter-Box',
+    },
+    {
+        title: 'Agora',
+        subtitle: 'Bootstrap, mysql, JS/HTML/CSS',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-5.png',
+        link: 'https://github.com/mcfoote/agoraWebForum',
+    },
+    {
+        title: 'MTG deck builder',
+        subtitle: 'JS/HTML/CSS, Bulma' ,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './project-6.png',
+        link: 'https://github.com/mcfoote/MTG-Deck-Builder',
+    },
+
 ]
