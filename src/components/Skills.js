3951e6f8302ea03e2335875234fe524486aaa8a2
Mitchell Foote <mcfoote321@gmail.com>
2023-01-30 15:50:51 -0500

    work on skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,1 +1,29 @@
-export default function Skills() {}
+import {BadgeCheckIcon, ChipIcon} from '@heroicicons/react/solid';
+import React from 'react';
+import {skills} from '../data';
+
+export default function Skills() {
+
+    return (
+        <section id="skills">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                    <ChipIcon className="w-10 inline-block mb-4" />
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                        Skills &amp; Technologies
+                    </h1>
+                    <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+
+                </div>
+            </div>
+        </section>
+    );
+
+}
