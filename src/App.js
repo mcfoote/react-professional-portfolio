fc195a06158c6e594ed4f7b6e52c63cc2586ac56
Mitchell Foote <mcfoote321@gmail.com>
2023-01-29 20:39:37 -0500

    work on about component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
 
     return (
 
-        <main>
+        <main className="text-gray-400 bg-gray-900 body-font">
             <Navbar />
             <About />
             <Projects />
--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,18 @@
-export default function About() {}
+import React from "react";
+
+export default function About() {
+
+    return( 
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hello, and welcome to Mitchell Foote's professional portfolio.
+                        <br className="hidden lg:inline-block"/> I am a software developer and built this site to showcase some of my projects.
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
+
+}
