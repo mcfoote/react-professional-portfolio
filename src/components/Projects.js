798d42b152e0ff83b9460fe771ef58b0832fb015
Mitchell Foote <mcfoote321@gmail.com>
2023-01-30 14:41:52 -0500

    work on projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,20 @@ export default function Projects() {
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className="flex flex-col w-full mb-20">
                     <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                        Software I have written.
+                    </h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="flex flex-wrap -m-4">
+                    {projects.map((project) => (
+                        
+                    ))}
                 </div>
             </div>
         </section>
