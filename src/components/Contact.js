40fd6ae13951b2d2d94af711fc785e99ed95acfc
Mitchell Foote <mcfoote321@gmail.com>
2023-01-31 20:50:44 -0500

    work on contact comp

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,11 @@
-export default function Contact() {}
+import React from 'react';
+
+export default function Contact() {
+
+    return (
+        <section id="contact" className="relative">
+
+        </section>
+    );
+
+}
