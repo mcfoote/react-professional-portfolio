b8152ff526a2229499e26730eb438dabb639717f
Mitchell Foote <mcfoote321@gmail.com>
2023-02-01 21:59:00 -0500

    work on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,18 @@ export default function Contact() {
 
     return (
         <section id="contact" className="relative">
-
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe 
+                        width="100%"
+                        height="100%"
+                        title="map"
+                        className="absolute inset-0"
+                        style={{ filter: "opacity(0.7)" }} 
+                        src=""
+                    />
+                </div>
+            </div>
         </section>
     );
 
