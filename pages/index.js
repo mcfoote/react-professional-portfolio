b3adda733fce05d59431f20f0151fc0d03c3b796
Mitchell Foote <mcfoote321@gmail.com>
2023-02-11 23:11:49 -0500

    add fontfam

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,12 @@ export default function Home() {
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl">Mitchell Foote</h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li>
-              <li><a className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+              </li>
             </ul>
           </nav>
         </section>
