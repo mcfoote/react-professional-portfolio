a9e8dd58e8c37e0568a874f94a2d09a57c6169ae
Mitchell Foote <mcfoote321@gmail.com>
2023-02-12 00:09:44 -0500

    work on body

--- pages/index.js
+++ pages/index.js
@@ -32,14 +32,14 @@ export default function Home() {
           <div className="text-center p-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium">Mitchell Foote</h2>
             <h3 className="text-2xl py-2">Fullstack Software Developer</h3>
-            <p>
+            <p className="text-md py-5 leading-6 text-gray-800">
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
           </div>
-          <div>
+          <div className="text-5xl flex justify-center gap-12">
             <AiFillGithub />
             <AiFillLinkedin />
           </div>
