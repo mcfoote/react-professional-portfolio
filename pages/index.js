07abf66bc8c7a87e6782bc07ec326a7e20d8e701
Mitchell Foote <mcfoote321@gmail.com>
2023-02-19 21:08:52 -0500

    work on styles

--- pages/index.js
+++ pages/index.js
@@ -40,12 +40,18 @@ export default function Home() {
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white"/>
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:from-purple-400 dark:to-purple-900" href="#portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:from-purple-400 dark:to-purple-900" href="#">Contact</a>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 dark:from-purple-400 dark:to-purple-900" href="#">Resume</a>
               </li>
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Mitchell Foote</h2>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-purple-700">Mitchell Foote</h2>
             <h3 className="text-2xl py-2 md:text-3xl dark:text-orange-600">Fullstack Software Developer</h3>
             <p className="text-md py-5 leading-6 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-orange-800">
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -80,7 +86,7 @@ export default function Home() {
               <li></li>
             </ul>
           </div>
-          <h3 className="text-center text-4xl py-4 dark:text-orange-600">Portfolio</h3>
+          <h3 id="portfolio" className="text-center text-4xl py-4 dark:text-orange-600">Portfolio</h3>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
               <Image src={project1} className="mx-auto rounded-lg "/>
@@ -91,7 +97,7 @@ export default function Home() {
                 the Bulma CSS framework is used to provide a polished interface which is responsive and scalable between mobile and desktop environments. 
                 Client-side storage is used to pass data between pages providing seamless functionality and moment.js api provides time data to anchor the site with a live environment feeling."
               </p>
-              <h4 className="py-4 text-teal-600">Technologies Used</h4>
+              <h4 className="py-4 text-teal-600 dark:text-purple-600">Technologies Used</h4>
               <p className="text-fray-800 py-1">HTML</p>
               <p className="text-fray-800 py-1">CSS</p>
               <p className="text-fray-800 py-1">Javascript</p>
@@ -104,7 +110,7 @@ export default function Home() {
                 aiming to be based on user generated content for now there are four boards users can browse post threads and comment on the threads. 
                 The site was buit using JS/HTML/CSS, Bootstrap, MySQL, Sequelize, Node.js, Expressjs."
               </p>
-              <h4 className="py-4 text-teal-600">Technologies Used</h4>
+              <h4 className="py-4 text-teal-600 dark:text-purple-600">Technologies Used</h4>
               <p className="text-fray-800 py-1">SQL</p>
               <p className="text-fray-800 py-1">Node</p>
               <p className="text-fray-800 py-1">Express.js</p>
@@ -118,7 +124,7 @@ export default function Home() {
                 Users can register an account which will be authenticated upon login, allowing the user to message friends in chat threads. 
                 Deployed using Heroku Web Services."
               </p>
-              <h4 className="py-4 text-teal-600">Technologies Used</h4>
+              <h4 className="py-4 text-teal-600 dark:text-purple-600">Technologies Used</h4>
               <p className="text-fray-800 py-1">MongoDB</p>
               <p className="text-fray-800 py-1">Graphql</p>
               <p className="text-fray-800 py-1">Node</p>
@@ -126,7 +132,7 @@ export default function Home() {
           </div>
         </section>
         <section>
-          <div className="text-center">
+          <div id="contact" className="text-center">
             <h3 className="text-center text-3xl font-medium pt-8 pb-2 dark:text-orange-600">Contact</h3>
             <p className="text-md py-5 leading-6 text-gray-800 dark:text-orange-800">
               Feel free to contact me at my email linkedin or github.
